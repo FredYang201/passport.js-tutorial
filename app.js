e44619191ab0ba089f52783d533e62d9ea97d058
FredYang201 <fredyang1402143595@gmail.com>
2020-11-12 21:22:23 -0800

    backing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const passport = require('passport')
 const app = express();
 
 // connect to online mongodb
-mongoose.connect(process.env.MONGODB_URI || keys.mongodb.dbURI,  { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(keys.mongodb.dbURI,  { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('Connected to mongodb')
 })
 
